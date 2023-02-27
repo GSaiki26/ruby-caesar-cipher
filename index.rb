3dbfa370cb43a070fc6395360cc0776d8b3dcf77
GSaiki26 <44827854+GSaiki26@users.noreply.github.com>
2023-02-27 04:30:24 -0300

    Updated: Basic Refactory

--- index.rb
+++ index.rb
@@ -3,14 +3,14 @@ class CaesarCipher
 
   def self.create(jump, text)
     new_text = ""
-    text.downcase.chars.collect do |char|
+    text.downcase.chars.each { |char|
       # Get the position from the chat in the alphabet.
       char_pos = @@alph.index(char)
       if !char_pos
         new_text += char
         next
       end
-      
+
       # Get the next char using the alphabet
       new_char_pos = char_pos + jump
       if new_char_pos >= @@alph.length
@@ -19,12 +19,13 @@ class CaesarCipher
 
       # Add the char to the text
       new_text +=  @@alph[new_char_pos]
-    end
+    }
+
     return new_text
   end
 end
 
 text = "Coe patraoz"
-(0..25).to_a.each do |i|
+25.times do |i|
   puts "Jump[#{i}]: '#{CaesarCipher.create(i, text)}'"
 end
